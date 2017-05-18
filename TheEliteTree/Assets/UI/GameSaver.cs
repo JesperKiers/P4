46ac45143d259dab43483150fb5b199a49007da2
AsherKlijnsmaDeltion <meneer.asher@live.nl>
2017-05-18 13:43:59 +0200

    tijdelijke update

---
+++ TheEliteTree/Assets/UI/GameSaver.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSaver : MonoBehaviour {
+    public Text tekst;
+    
+	// Use this for initialization
+	void Start () {
+
+        
+        if (PlayerPrefs.HasKey("test"))
+        {
+            print(PlayerPrefs.GetString("test"));
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Prefs();
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            tekst.text = PlayerPrefs.GetString("test");
+        }
+       
+    }
+    void Prefs()
+    {
+        PlayerPrefs.SetString("test", "dit is een test");
+    }
+}
+
